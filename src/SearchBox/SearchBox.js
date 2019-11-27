da5df03d4a3c2606886c31ea6f4703c2e00c7dbc
mohaiminalaoun <mohaimin.al.aoun@vanderbilt.edu>
2019-11-26 21:53:53 -0500

    Add search results initial

--- src/SearchBox/SearchBox.js
+++ src/SearchBox/SearchBox.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './SearchBox.css';
 
 const SearchBox = (props) => {
+  const [count, setCount] = useState(0);
+ // React hook that is the equivalent of componentDidMount, componentDidUpdate, and componentWillUnmount
+  useEffect(() => {
+    //console.log("useEffect in searchbox is called");
+    //setCount(count+1); // don't do this
+  });
   return <div className="search-container">
           <input
               id="searchbox"
