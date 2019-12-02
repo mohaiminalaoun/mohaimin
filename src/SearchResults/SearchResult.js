31b65b123a3b886149b825fc3d0efe3c0551fa46
mohaiminalaoun <mohaimin.al.aoun@vanderbilt.edu>
2019-12-01 21:31:09 -0500

    New icons work

--- src/SearchResults/SearchResult.js
+++ src/SearchResults/SearchResult.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import './SearchResult.css';
+import musicbar from './musicbar.svg';
+import college from './college.svg';
+import email from './email.svg';
+import books from './books.svg';
+import experience from './experience.svg';
+import projects from './projects.svg';
+import resume from './resume.svg';
 
 const SearchResult =  (props) => {
+
+  let src;
+      switch (props.icon) {
+        case 'musicbar':
+          src = musicbar;
+          break;
+        case 'college':
+          src = college;
+          break;
+        case 'email':
+          src = email;
+          break;
+        case 'books':
+          src = books;
+          break;
+        case 'experience':
+          src = experience;
+          break;
+        case 'projects':
+          src = projects;
+          break;
+        case 'resume':
+          src = resume;
+          break;
+        default:
+          src = email;
+      }
 
 
       return (
         <div className="result-container">
+        <img src={src} className="searchresult-image"alt=""/>
           <div className="result-text">{props.text}</div>
           <div className="result-desc"><a href={props.href}>{props.href}</a></div>
         </div>
--- src/data/suggestionsData.js
+++ src/data/suggestionsData.js
@@ -2,37 +2,44 @@ const suggestionItemsData = [
   {
     href: "https://www.linkedin.com/in/mohaiminalaoun",
     text: "What is his experience?",
-    tags: "job history work digital reasoning digitalreasoning vanderbilt vanderbiltuniversity microstrategy micro strategy"
+    tags: "job history work digital reasoning digitalreasoning vanderbilt vanderbiltuniversity microstrategy micro strategy",
+    icon: 'experience'
   },
   {
     href: "https://www.linkedin.com/in/mohaiminalaoun",
     text: "Mohaimin's resume",
-    tags: "school experience resume cv curriculum vitae"
+    tags: "school experience resume cv curriculum vitae",
+    icon: 'resume'
   },
   {
     href: "https://www.github.com/mohaiminalaoun",
     text: "What are some of his projects?",
-    tags: "work side projects experiments hobbies spare time"
+    tags: "work side projects experiments hobbies spare time",
+    icon: 'projects'
   },
   {
     href: "https://www.linkedin.com/in/mohaiminalaoun",
     text: "Where did he go to college?",
-    tags: "school study education vanderbilt vanderbiltuniversity computer science computerscience cs bachelor masters degree major minor graduate"
+    tags: "school study education vanderbilt vanderbiltuniversity computer science computerscience cs bachelor masters degree major minor graduate",
+    icon: 'college'
   },
   {
     href: "mailto:mohaimin.al.aoun@gmail.com",
     text: "How to contact Mohaimin?",
-    tags: "email phone send message letter"
+    tags: "email phone send message letter",
+    icon: 'email'
   },
   {
     href: "https://www.last.fm/user/ifElseReturn",
     text: "What music does he listen to?",
-    tags: "music songs listen hobby hobbies time spare guitar piano drums hear ear play"
+    tags: "music songs listen hobby hobbies time spare guitar piano drums hear ear play",
+    icon: 'musicbar'
   },
   {
     href: "https://www.goodreads.com/user/show/82712266-moh",
     text: "What books does he like reading?",
-    tags: "books book read hobby hobbies time spare fiction nonfiction study paperback pursue library"
+    tags: "books book read hobby hobbies time spare fiction nonfiction study paperback pursue library",
+    icon: "books"
   }
 ];
 
