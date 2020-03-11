a56151b9e5405cfb69f7af558e91ed0eb5803bd6
Mohaimin Al Aoun <mohaimin.al.aoun@vanderbilt.edu>
2020-03-10 23:09:52 -0400

    Add circle

--- src/Highlights/Highlights.js
+++ src/Highlights/Highlights.js
@@ -1,21 +1,35 @@
 import React from "react";
+import "./Highlight.css";
 
 const Highlight = ({ img }) => {
   console.log(img);
   const storyStyle = {
-    display: "inline-block",
-    marginLeft: "25px",
-    marginTop: "20px",
-    borderRadius: "30px",
-    height: "60px",
-    width: "60px",
-    border: "2px solid red",
-    backgroundColor: "white",
-    backgroundImage: `url("${img}")`,
-    backgroundSize: "cover",
-    opacity: 0.7
-  };
-  return <div style={storyStyle}></div>;
+      display: "inline-block",
+      marginLeft: "25px",
+      marginTop: "20px",
+      borderRadius: "30px",
+      height: "60px",
+      width: "60px",
+      backgroundColor: "white",
+      backgroundImage: `url("${img}")`,
+      backgroundSize: "cover"
+    },
+    backgroundStyle = {
+      border: "2px solid red",
+      borderRadius: "30px",
+      height: "57px",
+      width: "57px",
+      display: "inline-block",
+      position: "absolute",
+      marginLeft: "25px",
+      marginTop: "20px"
+    };
+  return (
+    <>
+      <div className={"highlight-background"} style={backgroundStyle}></div>
+      <div className={"highlight"} style={storyStyle}></div>
+    </>
+  );
 };
 
 export default Highlight;
